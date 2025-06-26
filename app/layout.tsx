0f9c5fe4b0a31de91cc9146a36ece7991ac79f41
Mo <mo.russolillo@halosbodycams.com>
2025-06-25 22:50:16 -0500

    Favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="../public/halos_favicon.svg" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
